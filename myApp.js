e1f91f22bea9ce66bd287d970597a020a512c6be
mayamauchi <root@DESKTOP-MSPGAUN.localdomain>
2022-11-01 21:38:32 -0500

    dotenv

--- myApp.js
+++ myApp.js
@@ -1,10 +1,16 @@
 let express = require('express');
 let app = express();
+require('dotenv').config();
 
+app.get('/', function (req, res) {
+res.sendFile(__dirname + "/views/index.html")
+}); 
 
+app.use("/public", express.static(__dirname + "/public"));
 
-
-
+app.get("/json", function (req, res) {
+    res.json({"message": "Hello json"})
+})
 
 
 
